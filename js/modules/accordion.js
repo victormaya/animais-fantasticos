02caa239c3387ed475a2ca74893d35a07b8d7f53
Victor Maya <victor.maya42@gmail.com>
2021-11-23 14:30:05 -0300

    refatorado fetch animasi

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,7 +1,7 @@
 export default class Accordion {
   constructor(list) {
     this.accordionList = document.querySelectorAll(list);
-    this.activeClass = "ativo";
+    this.activeClass = 'ativo';
   }
 
   toggleAccordion(item) {
@@ -9,19 +9,20 @@ export default class Accordion {
     item.nextElementSibling.classList.toggle(this.activeClass);
   }
 
-  //adiciona os eventos ao accordion
+  // adiciona os eventos ao accordion
   addAccordionEvent() {
     this.accordionList.forEach((item) => {
-      item.addEventListener("click", () => this.toggleAccordion(item));
+      item.addEventListener('click', () => this.toggleAccordion(item));
     });
   }
 
   // iniciar função
   init() {
     if (this.accordionList.length) {
-      //ativar primeiro item
+      // ativar primeiro item
       this.toggleAccordion(this.accordionList[0]);
       this.addAccordionEvent();
     }
+    return this;
   }
 }
